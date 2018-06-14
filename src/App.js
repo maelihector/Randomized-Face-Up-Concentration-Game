fdfcd0c55c414ad295625adcd11355d575735d54
mhector <maelihector@gmail.com>
2018-06-14 18:38:01 -0500

    Import components into App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component }from 'react';
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Wrapper from "./components/Wrapper/Wrapper";
+import GalaxyCard from "./components/GalaxyCard/GalaxyCard";
+import Footer from "./components/Footer/Footer";
 
-const App = () => (
+export default class App extends Component {
+  render() {
+    return (
 
-      <div className="app">
+    <div className="app">
+      <Header />
+      <Hero />
+      <Wrapper>
+        <GalaxyCard />
+      </Wrapper>
+      <Footer />
+    </div>
 
-      </div>
-
-);
-
+    )
+  }
+}
