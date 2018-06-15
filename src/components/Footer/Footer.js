ede1ce5c579c03f54c16da23097b9c012cccb63c
mhector <maelihector@gmail.com>
2018-06-14 21:39:07 -0500

    Add Footer HTML and CSS. Add react logo.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import "./Footer.css";
+import logo from "./../../logo.svg"; 
 
 export default class Footer extends Component {
   render() {
     return (
-      <div className="footer">
-        { this.props.children }
-      </div>
+  <footer className="footer">
+    <span>Concentration Game 2018<img src={logo} className="React-logo" alt="logo" /></span>
+  </footer>
     )
   }
 }
