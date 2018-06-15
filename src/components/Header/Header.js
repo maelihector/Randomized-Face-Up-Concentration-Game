69e9f610ca6a7e4e9685abb06a43b8a08713da9f
mhector <maelihector@gmail.com>
2018-06-14 19:21:45 -0500

    Add HTML and CSS to Header.js.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import './Header.css';
 
 export default class Header extends Component {
   render() {
     return (
-      <div className="header">
-        { this.props.children }
-      </div>
+      <nav className="navbar sticky-top navbar-dark">
+        <div className="container justify-content-center">
+          <div className="row align-items-center">
+
+            <div className="col">
+              Memory Game 
+            </div>
+            <div className="col">
+              Alert
+            </div>
+            <div className="col">
+              Score
+            </div>
+
+          </div>
+        </div>
+      </nav>
     )
   }
 }
