be882fb0db00f66fa6f9e2773f04b15dedcc431a
mhector <maelihector@gmail.com>
2018-06-16 08:21:01 -0500

    Add galaxies.sort function so galaxy images can randomize at every refreshing of the page.

--- src/App.js
+++ src/App.js
@@ -6,24 +6,28 @@ import GalaxyCard from "./components/GalaxyCard/GalaxyCard";
 import galaxies from "./galaxies.json";
 import Footer from "./components/Footer/Footer";
 
-export default class App extends Component {
-  render() {
-    return (
 
-    <div className="app">
-      <Header />
-      <Hero backgroundImage="https://www.nasa.gov/sites/default/files/images/613778main_hs-2011-18-a-xlarge_web_full.jpg">
-      </Hero>      
-      <Wrapper>
-      {galaxies.map( galaxy => <GalaxyCard
-          key={galaxy.name}
-          name={galaxy.name}
-          image={galaxy.image}
-          /> ) }
-      </Wrapper>
-      <Footer />
+export default class App extends Component {
+  
+  render() {
+    // randomize galaxy images at the refreshing of the page.
+    galaxies.sort(function(a, b){return 0.5 - Math.random()})
+    return (
+      <div className="app">
+        <Header />
+        <Hero backgroundImage="https://www.nasa.gov/sites/default/files/images/613778main_hs-2011-18-a-xlarge_web_full.jpg">
+        </Hero>      
+        <Wrapper>
+          {galaxies.map( galaxy =>  (<GalaxyCard
+            key={galaxy.id}
+            name={galaxy.name}
+            image={galaxy.image}
+            clicked={galaxy.clicked}
+          /> ))}
+        </Wrapper>
+        <Footer />
     </div>
 
-    )
+    );
   }
 }
