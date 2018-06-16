8170944fd70c072522261bb9ce6f5a888cacede7
mhector <maelihector@gmail.com>
2018-06-16 08:24:29 -0500

    Add  event for changing state of image from false to true when clicked. Add galaxies.sort to sort images after every click, but the randomized galaxies are only console.logged, and do not yet affect the cards on the DOM.

--- src/components/GalaxyCard/GalaxyCard.js
+++ src/components/GalaxyCard/GalaxyCard.js
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
 import './GalaxyCard.css';
+import galaxies from '../../galaxies.json';
+
 
 export default class GalaxyCard extends Component {
+  
+  state = {
+    value: this.props.clicked,
+    
+  }
+  
+  handleImageValueChange = event => {
+    const value = this.state.value
+    
+    if (value) {
+      console.log(`This is: ${value}`);
+      galaxies.sort(function(a, b){return 0.5 - Math.random()});
+      galaxies.map( galaxy =>  (galaxies))
+      // This sorts the images, but their values are reset back to false.
+      console.log(galaxies);
+  
+      // correctAlert();
+      this.setState ({
+        value: true
+      });
+      console.log(`New state: ${value}`);
+      
+    } else {
+      // topScoreKeeper();
+      // incorrectAlert();
+      galaxies.sort(function(a,b){return 0.5 - Math.random()});
+      galaxies.map( galaxy =>  (galaxies))
+    }
+  }
+  
   render() {
+    
     return (
-      <div className="card">
+      <div className="card"
+        value={this.state.clicked}
+        onClick={this.handleImageValueChange}>
         <div className="img-container">
-          <img alt={this.props.name} src={this.props.image} />
+          <img 
+          alt={this.props.name} 
+          src={this.props.image}
+          />
+          { this.props.children }
         </div>
       </div>
     )
